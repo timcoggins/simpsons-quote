a8c317ea929f4123dde837e3f35cc2068fb76d0e
Tim Coggins <timcoggins@gmail.com>
2021-10-25 10:05:51 +0200

    Initial commit

---
+++ simpsons-quote/src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import axios from "axios";
+import QuoteCard from "./components/QuoteCard";
+import {useState} from "react";
+
+const initialQuote = {
+    quote: "Gah, stupid sexy Flanders!",
+    character: "Homer Simpson",
+    image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+    characterDirection: "Right"
+}
+
+function App() {
+  const [quote, setQuote] = useState(initialQuote);
+
+  const getQuote = () => {
+      // Send the request
+      axios
+          .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+          // Extract the DATA from the received response
+          .then((response) => response.data)
+          // Use this data to update the state
+          .then((data) => {
+              console.log(data[0])
+              setQuote(data[0]);
+
+          });
+  }
+
+  return (
+    <div className="App">
+      <QuoteCard quote={quote} />
+      <button onClick={getQuote}>Get a New Quote</button>
+    </div>
+  );
+}
+
+export default App;
